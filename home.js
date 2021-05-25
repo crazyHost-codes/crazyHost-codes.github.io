bdfd457fdbd3b2acdfc921d9d483f16521a3f442
Belarmino Adriano <73590045+crazyHost-codes@users.noreply.github.com>
2021-05-25 17:27:14 +0100

    Add files via upload

---
+++ home.js
@@ -0,0 +1,14 @@
+var MenuItem = document.getElementById("MenuItems");
+
+        MenuItem.style.maxHeight = "0px";
+
+        function menutoggle() {
+            if (MenuItem.style.maxHeight == "0px")
+                {
+                    MenuItem.style.maxHeight == "200px";
+                }
+            else
+                {
+                    MenuItem.style.maxHeight == "0px";
+                }
+        }
